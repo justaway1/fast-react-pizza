d022abd3bd3bdf52318b76b24bc8b3098a81da42
Stanislav Angelov <stannyweb@gmail.com>
2024-02-27 10:04:54 +0200

    added The main Layout view

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,15 @@
-function CartOverview() {
+import { Link } from 'react-router-dom'
+
+function CartOverview () {
   return (
     <div>
       <p>
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <a href="#">Open cart &rarr;</a>
+      <Link to='/cart'>Open cart &rarr;</Link>
     </div>
-  );
+  )
 }
 
-export default CartOverview;
+export default CartOverview
