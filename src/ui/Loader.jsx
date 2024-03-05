f3c812d0cf4197b9aa887ca16d9ff8b648bb67ad
Stanislav Angelov <stannyweb@gmail.com>
2024-03-05 19:04:31 +0200

    added loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,9 @@
-function Loader () {
-  return <div className='loader'></div>
+function Loader() {
+  return (
+    <div className="absolute inset-0 bg-stone-200/25 flex items-center justify-center backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
